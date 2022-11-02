2073d8db24adb14d0c7bdeb73d7c4ce2c5f344c2
MYorston <mddyorston@gmail.com>
2022-11-01 23:58:18 -0300

    added tab, list, and item components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule

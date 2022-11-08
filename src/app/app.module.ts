601d3131e0735fa723bab879529f73a963aa47bf
MYorston <mddyorston@gmail.com>
2022-11-07 22:16:01 -0400

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
@@ -8,6 +9,17 @@ import { ListComponent } from './list/list.component';
 import { ItemsComponent } from './items/items.component';
 import { StarWarsService } from './star-wars.service';
 import { CreateCharacterComponent } from './create-character/create-character.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  { path: 'characters', component: TabsComponent, children: [
+      { path: '', redirectTo: 'all', pathMatch: 'full' },
+      { path: ':side', component: ListComponent }
+    ]
+  },
+  { path: 'new-character', component: CreateCharacterComponent },
+  { path: '**', redirectTo: '/characters' }
+];
 
 @NgModule({
   declarations: [
@@ -15,11 +27,13 @@ import { CreateCharacterComponent } from './create-character/create-character.co
     TabsComponent,
     ListComponent,
     ItemsComponent,
-    CreateCharacterComponent
+    CreateCharacterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [StarWarsService],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Character } from '../app.modal'
+import { StarWarsService } from '../star-wars.service'
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  @Input() characters: Character[] = [];
+  characters: Character[] = [];
   
-  constructor() { }
+  constructor(private router: ActivatedRoute, private starWarsService: StarWarsService) { }
 
   ngOnInit(): void {
-  }
+    this.router.params.subscribe( (params) =>{
+      this.characters = this.starWarsService.getCharacters(params['side']);
+    })
+  } 
 }
